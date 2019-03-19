553311407dc39cae4a93d8ae8a1b7868c8919e27
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-03-19 12:51:22 +0900

    Allow to Set the region after client creation

--- new.go
+++ new.go
@@ -16,15 +16,26 @@ func New(options ClientOptions) *Client {
 	if len(options.Region) == 0 {
 		options.Region = "mypurecloud.com"
 	}
-	apiURL, err := url.Parse(fmt.Sprintf("https://api.%s/api/v2/", options.Region))
-	if err != nil {
-		apiURL, _ = url.Parse("https://api.mypurecloud.com/api/v2/")
-	}
 	return &Client{
-		Region:       options.Region,
-		API:          apiURL,
-		Organization: &Organization{},
-		DeploymentID: options.DeploymentID,
-		Logger:       options.Logger,
+		Region:        options.Region,
+		API:           getAPI(options.Region),
+		Organization:  &Organization{},
+		Authorization: &Authorization{GrantType: "ClientCredentials"},
+		DeploymentID:  options.DeploymentID,
+		Logger:        options.Logger,
 	}
+}
+
+// SetRegion sets the region and its main API
+func (client *Client) SetRegion(region string) {
+	client.Region = region
+	client.API    = getAPI(region)
+}
+
+func getAPI(region string) *url.URL {
+	if api, err := url.Parse(fmt.Sprintf("https://api.%s/api/v2/", region)); err == nil {
+		return api
+	}
+	api, _ := url.Parse("https://api.mypurecloud.com/api/v2/")
+	return api
 }
