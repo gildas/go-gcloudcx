9d568426685bb23f2bfb2886e1ec189b902540d8
Gildas Cherruel <gildas@breizh.org>
2019-03-22 23:17:01 +0900

    Added json fields

--- types.go
+++ types.go
@@ -9,13 +9,13 @@ import (
 
 // Client is the primary object to use PureCloud
 type Client struct {
-	Region         string
-	Organization   *Organization
-	DeploymentID   string
-	API            *url.URL
-	Proxy          *url.URL
-	Authorization  *Authorization
-	Logger         *logger.Logger
+	Region         string         `json:"region"`
+	Organization   *Organization  `json:"organization"`
+	DeploymentID   string         `json:"deploymentId"`
+	API            *url.URL       `json:"apiUrl"`
+	Proxy          *url.URL       `json:"proxyUrl"`
+	Authorization  *Authorization `json:"auth"`
+	Logger         *logger.Logger `json:"-"`
 }
 
 // ClientOptions contains the options to create a new Client
@@ -28,10 +28,10 @@ type ClientOptions struct {
 
 // Authorization contains the login options to connect the client to PureCloud
 type Authorization struct {
-	GrantType    string
-	ClientID     string
-	Secret       string
-	TokenType    string
-	Token        string
-	TokenExpires time.Time
+	GrantType    string    `json:"grantType"`
+	ClientID     string    `json:"clientId"`
+	Secret       string    `json:"clientSecret"`
+	TokenType    string    `json:"tokenType"`
+	Token        string    `json:"token"`
+	TokenExpires time.Time `json:"tokenExpires"`
 }
