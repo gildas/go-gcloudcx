827dc4e059bd9e387059482308879b0f15186cfb
Gildas <gildas@breizh.org>
2019-10-15 20:16:08 +0900

    Unused authorization grant

--- auth.go
+++ auth.go
@@ -1,32 +1,8 @@
 package purecloud
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/pkg/errors"
-)
-
 // AuthorizationGrant describes the capabilities authorization grants must have
 type AuthorizationGrant interface {
 	// Authorize this Grant with PureCloud
 	Authorize(client *Client) error
 	AccessToken() *AccessToken
-}
-
-type key int
-
-// GrantContextKey is the key for logger child stored in Context
-const GrantContextKey key = iota
-
-// AuthorizationGrantFromContext retrieves a grant from an HTTP Context
-func AuthorizationGrantFromContext(context context.Context) (AuthorizationGrant, error) {
-	grant := context.Value(GrantContextKey)
-	if grant == nil {
-		return nil, fmt.Errorf("Context does not contain any grant")
-	}
-	if implictGrant, ok := grant.(*ImplicitGrant); ok {
-		return implictGrant, nil
-	}
-	return nil, errors.New("Unsupported Authorization Grant")
 }
