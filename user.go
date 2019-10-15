eeae9e8118203957e3492223d936bc9a3b179e4f
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-10-15 18:25:28 +0900

    Added new fields to user

--- user.go
+++ user.go
@@ -3,6 +3,33 @@ package purecloud
 // User describe a PureCloud User
 type User struct {
 	ID                         string          `json:"id"`
+	Name string `json:"name"`
+	Division struct {
+		ID      string `json:"id"`
+		Name    string `json:"name"`
+		SelfURI string `json:"selfUri"`
+	} `json:"division"`
+	Chat struct {
+		JabberID string `json:"jabberId"`
+	} `json:"chat"`
+	Department string `json:"department"`
+	Mail       string `json:"email"`
+	PrimaryContactInfo []struct{
+		Address string `json:"address"`
+		MediaType string `json:"mediaType"`
+		Type      string `json:"type"`
+	} `json:"primaryContactInfo"`
+	Addresses []struct {
+		Display string `json:"display"`
+		MediaType string `json:"mediaType"`
+		Type      string `json:"type"`
+	} `json:"addresses"`
+	State  string `json:"state"`
+	Title string `json:"title"`
+	UserName string `json:"username"`
+	Version int `json:"version"`
+	AcdAutoAnswer bool `json:"acdAutoAnswer"`
+	SelfURI string `json:"selfUri"`
 }
 
 func (client *Client) GetMyUser() (*User, error) {
