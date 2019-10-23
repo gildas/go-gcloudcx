49994d6d105573380ba096ea0bfbef28f016fe6f
Gildas <gildas@breizh.org>
2019-10-24 00:33:22 +0900

    Removed unused http handler

--- examples/chat_bot/route_login.go
+++ examples/chat_bot/route_login.go
@@ -37,34 +37,3 @@ func LoggedInHandler() http.Handler {
 		}
 	})
 }
-
-// LoginHandler validates the various variables in the Application
-func LoginHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		log := logger.Must(logger.FromContext(r.Context())).Topic("route").Scope("login")
-		appConfig, _ := AppConfigFromContext(r.Context())
-
-		client, err := purecloud.ClientFromContext(r.Context())
-		if err != nil {
-			log.Errorf("Failed to retrieve the PureCloud Client", err)
-			core.RespondWithError(w, http.StatusServiceUnavailable, err)
-			return
-		}
-
-		client.Organization, _ = client.GetMyOrganization()
-
-		if err = appConfig.Initialize(client); err != nil {
-			log.Errorf("Failed to Initialize App Config", err)
-			core.RespondWithError(w, http.StatusServiceUnavailable, err)
-			return
-		}
-		if len(appConfig.WebRootPath) > 0 {
-			log.Infof("Redirecting to %s", appConfig.WebRootPath)
-			http.Redirect(w, r, appConfig.WebRootPath, http.StatusTemporaryRedirect)
-		} else {
-			log.Infof("Redirecting to /")
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		}
-	})
-
-}
