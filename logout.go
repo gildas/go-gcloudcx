a66d3ac9c75758f914d01ae84260e3a7fd1780cc
Gildas <gildas@breizh.org>
2019-10-23 23:14:21 +0900

    always logout

--- logout.go
+++ logout.go
@@ -1,10 +1,9 @@
 package purecloud
 
 // Logout logs out a Client from PureCloud
-func (client *Client) Logout() (err error) {
-	if err := client.Delete("/tokens/me", nil); err != nil { return err }
+func (client *Client) Logout() {
+	client.Delete("/tokens/me", nil) // we don't care much about the error as we are logging out
 	if client.AuthorizationGrant != nil {
 		client.AuthorizationGrant.AccessToken().Reset()
 	}
-	return nil
 }
