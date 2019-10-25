37f1bbaf8d9776f5a1c083c5f4244acd3aa9d28a
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-10-25 16:33:08 +0900

    Wrapup Marshal/Unmarshal

--- wrapup.go
+++ wrapup.go
@@ -1,7 +1,10 @@
 package purecloud
 
 import (
-	"time"
+	"encoding/json"
+  "time"
+
+	"github.com/pkg/errors"
 )
 
 // Wrapup describes a Wrapup
@@ -10,7 +13,35 @@ type Wrapup struct {
   Code        string        `json:"code"`
   Notes       string        `json:"notes"`
   Tags        []string      `json:"tags"`
-  Duration    time.Duration `json:"durationSeconds"` // time.Duration
+  Duration    time.Duration `json:"-"`
   EndTime     time.Time     `json:"endTime"`
   Provisional bool          `json:"provisional"`
+}
+
+// MarshalJSON marshals this into JSON
+func (wrapup Wrapup) MarshalJSON() ([]byte, error) {
+  type surrogate Wrapup
+	return json.Marshal(struct {
+    surrogate
+		D          int64   `json:"durationSeconds"`
+	}{
+    surrogate: surrogate(wrapup),
+		D:         int64(wrapup.Duration.Seconds()),
+	})
+}
+
+// UnmarshalJSON unmarshals JSON into this
+func (wrapup *Wrapup) UnmarshalJSON(payload []byte) (err error) {
+  type surrogate Wrapup
+	var inner struct {
+    surrogate
+		D          int64   `json:"durationSeconds"`
+	}
+
+	if err = json.Unmarshal(payload, &inner); err != nil {
+		return errors.WithStack(err)
+  }
+  *wrapup = Wrapup(inner.surrogate)
+	wrapup.Duration = time.Duration(inner.D) * time.Second
+	return
 }
