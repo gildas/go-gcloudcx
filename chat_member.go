b0e369ce84a7fb7cbca7505f6b340e661405ccd7
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-10-28 14:53:28 +0900

    Use net/url for urls

--- chat_member.go
+++ chat_member.go
@@ -1,13 +1,18 @@
 package purecloud
 
 import (
+	"encoding/json"
+	"net/url"
 	"time"
+
+	"github.com/gildas/go-core"
+	"github.com/pkg/errors"
 )
 
 type ChatMember struct {
 	ID            string            `json:"id,omitempty"`
 	DisplayName   string            `json:"displayName,omitempty"`
-	AvatarURL     string            `json:"avatarImageUrl,omitempty"`
+	AvatarURL     *url.URL          `json:"-"`
 	Role          string            `json:"role,omitempty"`
 	State         string            `json:"state,omitempty"`
 	JoinedAt      time.Time         `json:"joinDate,omitempty"`
@@ -29,4 +34,31 @@ func (member ChatMember) String() string {
 		return member.DisplayName
 	}
 	return member.ID
+}
+
+// MarshalJSON marshals this into JSON
+func (member ChatMember) MarshalJSON() ([]byte, error) {
+	type surrogate ChatMember
+	return json.Marshal(struct {
+		surrogate
+		A *core.URL `json:"avatarImageUrl,omitempty"`
+	}{
+		surrogate: surrogate(member),
+		A:         (*core.URL)(member.AvatarURL),
+	})
+}
+
+// UnmarshalJSON unmarshals JSON into this
+func (member *ChatMember) UnmarshalJSON(payload []byte) (err error) {
+	type surrogate ChatMember
+	var inner struct {
+		surrogate
+		A *core.URL `json:"avatarImageUrl,omitempty"`
+	}
+	if err = json.Unmarshal(payload, &inner); err != nil {
+		return errors.WithStack(err)
+	}
+	*member = ChatMember(inner.surrogate)
+	member.AvatarURL = (*url.URL)(inner.A)
+	return
 }
