25775394cc43592984a88a22a3c58238ba530f6b
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-10-28 14:54:11 +0900

    Conversation Guest Chat is done

--- conversation_guestchat.go
+++ conversation_guestchat.go
@@ -1,9 +1,14 @@
 package purecloud
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"reflect"
+	"strings"
+	"time"
 
+	"github.com/gildas/go-core"
 	"github.com/gildas/go-logger"
 	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
@@ -11,80 +16,83 @@ import (
 
 // ConversationGuestChat describes a Guest Chat
 type ConversationGuestChat struct {
-	ID             string `json:"id"`
-	SelfURI        string `json:"selfUri,omitempty"`
-
-	Target         *RoutingTarget         `json:"-"`
-	Guest          *ChatMember            `json:"member,omitempty"`
-	Members        map[string]*ChatMember `json:"-"`
-
-	JWT             string          `json:"jwt,omitempty"`
-	EventStream     string          `json:"eventStreamUri,omitempty"`
-	Socket          *websocket.Conn `json:"-"`
-	Client          *Client         `json:"-"`
-	Logger          *logger.Logger  `json:"-"`
+	ID             string                  `json:"id"`
+	SelfURI        string                  `json:"selfUri,omitempty"`
+	Target         *RoutingTarget          `json:"-"`
+	Guest          *ChatMember             `json:"member,omitempty"`
+	Members        map[string]*ChatMember  `json:"-"`
+	JWT             string                 `json:"jwt,omitempty"`
+	EventStream     string                 `json:"eventStreamUri,omitempty"`
+	Socket          *websocket.Conn        `json:"-"`
+	TopicReceived   chan NotificationTopic `json:"-"`
+	LogHeartbeat    bool                   `json:"logHeartbeat"`
+	Client          *Client                `json:"-"`
+	Logger          *logger.Logger         `json:"-"`
 }
 
 // Initialize initializes this from the given Client
 //   implements Initializable
 func (conversation *ConversationGuestChat) Initialize(parameters ...interface{}) (err error) {
+	client := conversation.Client
+	log    := conversation.Logger
+	guest  := conversation.Guest
+	target := conversation.Target
 	for _, parameter := range parameters {
 		if paramClient, ok := parameter.(*Client); ok {
-			conversation.Client = paramClient
+			client = paramClient
 		}
 		if paramLogger, ok := parameter.(*logger.Logger); ok {
-			conversation.Logger = paramLogger.Topic("conversation").Scope("conversation").Record("media", "chat")
+			log = paramLogger.Topic("conversation").Scope("conversation").Record("media", "chat")
 		}
 		if paramGuest, ok := parameter.(*ChatMember); ok {
-			conversation.Guest = paramGuest
+			guest = paramGuest
 		}
 		if paramTarget, ok := parameter.(*RoutingTarget); ok {
-			conversation.Target = paramTarget
+			target = paramTarget
 		}
 	}
-	if conversation.Client == nil {
+	if client == nil {
 		return errors.Errorf("Missing Client in initialization of %s %s", reflect.TypeOf(conversation).String(), conversation.GetID())
 	}
-	if conversation.Logger == nil {
-		conversation.Logger = conversation.Client.Logger.Topic("conversation").Scope("conversation").Record("media", "chat")
+	if log == nil {
+		log = conversation.Client.Logger
 	}
-	if conversation.Guest == nil {
+	if guest == nil {
 		return errors.New("Missing ChatMember guest")
 	}
-	if conversation.Target == nil {
+	if target == nil {
 		return errors.New("Missing ChatMember Target")
 	}
-	if conversation.Client.Organization == nil {
+	if client.Organization == nil {
 		return errors.New("Missing Organization in Client")
 	}
-	if len(conversation.Client.DeploymentID) == 0 {
+	if len(client.DeploymentID) == 0 {
 		return errors.New("Missing Deployment ID in Client")
 	}
-	conversation.Members = map[string]*ChatMember{}
 
-	if err = conversation.Client.Post("/webchat/guest/conversations",
+	if err = client.Post("/webchat/guest/conversations",
 		struct {
 			OrganizationID string         `json:"organizationId"`
 			DeploymentID   string         `json:"deploymentId"`
 			RoutingTarget  *RoutingTarget `json:"routingTarget"`
 			Guest          *ChatMember    `json:"memberInfo"`
 		}{
-			OrganizationID: conversation.Client.Organization.ID,
-			DeploymentID:   conversation.Client.DeploymentID,
-			RoutingTarget:  conversation.Target,
-			Guest:          conversation.Guest,
+			OrganizationID: client.Organization.ID,
+			DeploymentID:   client.DeploymentID,
+			RoutingTarget:  target,
+			Guest:          guest,
 		},
 		&conversation,
 	); err != nil {
 		return errors.WithStack(err)
 	}
-	conversation.Logger = conversation.Logger.Record("conversation", conversation.ID)
-	conversation.Members[conversation.Guest.GetID()] = conversation.Guest
-
-	conversation.Socket, _, err = websocket.DefaultDialer.Dial(conversation.EventStream, nil)
-	if err != nil {
-		conversation.Close()
-	}
+	conversation.Client            = client
+	conversation.Logger            = log.Topic("conversation").Scope("conversation").Record("media", "chat").Record("conversation", conversation.ID)
+	conversation.Guest             = guest
+	conversation.Members           = map[string]*ChatMember{}
+	conversation.Members[guest.ID] = guest
+	conversation.TopicReceived     = make(chan NotificationTopic)
+	conversation.LogHeartbeat      = core.GetEnvAsBool("PURECLOUD_LOG_HEARTBEAT", false)
 	return
 }
 
@@ -100,6 +108,22 @@ func (conversation ConversationGuestChat) String() string {
 	return conversation.ID
 }
 
+// Connect connects a Guest Chat to its websocket and starts its message loop
+//   If the websocket was already connected, nothing happens
+//   If the environment variable PURECLOUD_LOG_HEARTBEAT is set to true, the Heartbeat topic will be logged
+func (conversation *ConversationGuestChat) Connect() (err error) {
+	if conversation.Socket != nil {
+		return
+	}
+	conversation.Socket, _, err = websocket.DefaultDialer.Dial(conversation.EventStream, nil)
+	if err != nil {
+		conversation.Close()
+	}
+	go conversation.messageLoop()
+	return
+}
+
+// Close disconnects the websocket and the guest
 func (conversation *ConversationGuestChat) Close() (err error) {
 	log := conversation.Logger.Scope("close")
 
@@ -118,6 +142,149 @@ func (conversation *ConversationGuestChat) Close() (err error) {
 			return errors.WithStack(err)
 		}
 		log.Infof("Disconnected Guest Member")
+	}
+	return
+}
+
+func (conversation *ConversationGuestChat) messageLoop() (err error) {
+	log := conversation.Logger.Scope("receive")
+
+	if conversation.Socket == nil {
+		return errors.New("Conversation Not Connected")
+	}
+
+	for {
+		// get a message body and decode it. (ReadJSON is nice, but in case of unknown message, I cannot get the original string)
+		var body []byte
+
+		if _, body, err = conversation.Socket.ReadMessage(); err != nil {
+			if strings.Contains(err.Error(), "use of closed network connection") {
+				log.Infof("Websocket was closed, stopping receive handler")
+				return nil
+			}
+			log.Errorf("Failed to read incoming message", err)
+			continue
+		}
+
+		// we have to use a custom version since topics are the same between agent chats and guest chats
+		// TODO: We should create a Conversation interface...
+		topic, err := conversation.notificationTopicFromJSON(body)
+		if err != nil {
+			log.Warnf("%s, Body size: %d, Content: %s", err.Error(), len(body), string(body))
+			continue
+		}
+		switch topic.(type) {
+		case *MetadataTopic:
+			if conversation.LogHeartbeat {
+				log.Tracef("Request %d bytes: %s", len(body), string(body))
+			}
+		default:
+			log.Tracef("Request %d bytes: %s", len(body), string(body))
+		}
+		// Make a fake channel object so Notification Topics can be sent through
+		topic.Send(&NotificationChannel{
+			ID:            conversation.ID,
+			LogHeartbeat:  conversation.LogHeartbeat,
+			Logger:        conversation.Logger,
+			Client:        conversation.Client,
+			Socket:        conversation.Socket,
+			TopicReceived: conversation.TopicReceived,
+		})
+	}
+}
+
+func (conversation *ConversationGuestChat) notificationTopicFromJSON(payload []byte) (NotificationTopic, error) {
+	var header struct {
+		TopicName string `json:"topicName"`
+		Data      json.RawMessage
+	}
+	if err := json.Unmarshal(payload, &header); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	switch {
+	case ConversationGuestChatMessageTopic{}.Match(header.TopicName):
+		var topic ConversationGuestChatMessageTopic
+		if err := json.Unmarshal(payload, &topic); err != nil {
+			return nil, errors.WithStack(err)
+		}
+		return &topic, nil
+	case ConversationGuestChatMemberTopic{}.Match(header.TopicName):
+		var topic ConversationGuestChatMemberTopic
+		if err := json.Unmarshal(payload, &topic); err != nil {
+			return nil, errors.WithStack(err)
+		}
+		return &topic, nil
+	case MetadataTopic{}.Match(header.TopicName):
+		var topic MetadataTopic
+		if err := json.Unmarshal(payload, &topic); err != nil {
+			return nil, errors.WithStack(err)
+		}
+		return &topic, nil
+	default:
+		return nil, errors.Errorf("Unsupported Topic: %s", header.TopicName)
+	}
+}
+
+// SendTyping sends a typing indicator to PureCloud as the chat guest
+func (conversation *ConversationGuestChat) SendTyping() (err error) {
+	response := &struct {
+		ID           string       `json:"id,omitempty"`
+		Name         string       `json:"name,omitempty"`
+		Conversation Conversation `json:"conversation,omitempty"`
+		Sender       *ChatMember  `json:"sender,omitempty"`
+		Timestamp    time.Time    `json:"timestamp,omitempty"`
+	}{}
+	if err = conversation.Client.SendRequest(
+		fmt.Sprintf("/webchat/guest/conversations/%s/members/%s/typing", conversation.ID, conversation.Guest.ID),
+		&core.RequestOptions{
+			Method:        http.MethodPost, // since payload is empty
+			Authorization: "bearer " + conversation.JWT,
+		},
+		&response,
+	); err == nil {
+		conversation.Client.Logger.Record("scope", "sendtyping").Infof("Sent successfuly. Response: %+v", response)
+	}
+	return
+}
+
+// SendMessage sends a message as the chat guest
+func (conversation *ConversationGuestChat) SendMessage(text string) (err error) {
+	return conversation.sendBody("standard", text)
+}
+
+// SendNotice sends a notice as the chat guest
+func (conversation *ConversationGuestChat) SendNotice(text string) (err error) {
+	return conversation.sendBody("notice", text)
+}
+
+// sendBody sends a body message as the chat guest
+func (conversation *ConversationGuestChat) sendBody(bodyType, body string) (err error) {
+	response := &struct {
+		ID           string       `json:"id,omitempty"`
+		Name         string       `json:"name,omitempty"`
+		Conversation Conversation `json:"conversation,omitempty"`
+		Sender       *ChatMember  `json:"sender,omitempty"`
+		Body         string       `json:"body,omitempty"`
+		BodyType     string       `json:"bodyType,omitempty"`
+		Timestamp    time.Time    `json:"timestamp,omitempty"`
+		SelfURI      string       `json:"selfUri,omitempty"`
+	}{}
+	if err = conversation.Client.SendRequest(
+		fmt.Sprintf("/webchat/guest/conversations/%s/members/%s/messages", conversation.ID, conversation.Guest.ID),
+		&core.RequestOptions{
+			Authorization: "bearer " + conversation.JWT,
+			Payload:       struct {
+				BodyType string `json:"bodyType"`
+				Body     string `json:"body"`
+			}{
+				BodyType: bodyType,
+				Body:     body,
+
+			},
+		},
+		&response,
+	); err == nil {
+		conversation.Client.Logger.Record("scope", "sendbody").Infof("Sent successfuly. Response: %+v", response)
 	}
 	return
 }
---
+++ notification_topic_conv_guestchat_message.go
@@ -0,0 +1,90 @@
+package purecloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// ConversationGuestChatMessageTopic describes a Topic about User's Presence
+type ConversationGuestChatMessageTopic struct {
+	ID             string
+	Name           string
+	Conversation   *ConversationGuestChat
+	Sender         *ChatMember
+	Type           string     // message, typing-indicator, 
+	Body           string
+	BodyType       string     // standard,
+	TimeStamp      time.Time
+	CorrelationID  string
+	Client         *Client
+}
+
+// Match tells if the given topicName matches this topic
+func (topic ConversationGuestChatMessageTopic) Match(topicName string) bool {
+	return strings.HasPrefix(topicName, "v2.conversations.chats.") && strings.HasSuffix(topicName, ".messages")
+}
+
+// Get the PureCloud Client associated with this
+func (topic *ConversationGuestChatMessageTopic) GetClient() *Client {
+	return topic.Client
+}
+
+// TopicFor builds the topicName for the given identifiables
+func (topic ConversationGuestChatMessageTopic) TopicFor(identifiables ...Identifiable) string {
+	if len(identifiables) > 0 {
+		return fmt.Sprintf("v2.conversations.chats.%s.messages", identifiables[0].GetID())
+	}
+	return ""
+}
+
+// Send sends the current topic to the Channel's chan
+func (topic *ConversationGuestChatMessageTopic) Send(channel *NotificationChannel) {
+	log := channel.Logger.Topic("conversation_chat_message").Scope("send")
+	log.Debugf("Conversation: %s, Type: %s, Body Type: %s, Sender: %s", topic.Conversation, topic.Type, topic.BodyType, topic.Sender)
+	topic.Client              = channel.Client
+	topic.Conversation.Client = channel.Client
+	channel.TopicReceived <- topic
+}
+
+// UnmarshalJSON unmarshals JSON into this
+func (topic *ConversationGuestChatMessageTopic) UnmarshalJSON(payload []byte) (err error) {
+	var inner struct {
+		TopicName string       `json:"topicName"`
+		EventBody struct {
+			ID           string                 `json:"id,omitempty"`
+			Conversation *ConversationGuestChat `json:"conversation,omitempty"`
+			Sender       *ChatMember            `json:"sender,omitempty"`
+			Body         string                 `json:"body,omitempty"`
+			BodyType     string                 `json:"bodyType,omitempty"`
+			Timestamp    time.Time              `json:"timestamp,omitempty"`
+		} `json:"eventBody"`
+		Metadata struct {
+			CorrelationID string `json:"correlationId,omitempty"`
+			Type          string `json:"type,omitempty"`
+		} `json:"metadata,omitempty"`
+		Version   string `json:"version"` // all
+	}
+	if err = json.Unmarshal(payload, &inner); err != nil {
+		return errors.WithStack(err)
+	}
+	conversationID := strings.TrimSuffix(strings.TrimPrefix(inner.TopicName, "v2.conversations.chats."), ".messages")
+	topic.Name          = inner.TopicName
+	topic.Type          = inner.Metadata.Type
+	topic.Conversation  = &ConversationGuestChat{ID:conversationID}
+	topic.Sender        = inner.EventBody.Sender
+	topic.BodyType      = inner.EventBody.BodyType
+	topic.Body          = inner.EventBody.Body
+	topic.TimeStamp     = inner.EventBody.Timestamp
+	topic.CorrelationID = inner.Metadata.CorrelationID
+	return
+}
+
+// String gets a string version
+//   implements the fmt.Stringer interface
+func (topic ConversationGuestChatMessageTopic) String() string {
+	return fmt.Sprintf("%s=%s", topic.Name, topic.Sender)
+}
