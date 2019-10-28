5235787864e9df4993d413bfb4343435d4e486e5
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-10-28 12:47:50 +0900

    Missing / at the end of Matt's service

--- examples/chat_bot/message_loop.go
+++ examples/chat_bot/message_loop.go
@@ -118,7 +118,7 @@ func MessageLoop(config* AppConfig) {
 						EndConversation bool    `json:"end_conversation"` 
 					}{EndConversation: false}
 					if _, err = core.SendRequest(&core.RequestOptions{
-							URL: &url.URL{Scheme: "https", Host: "newpod-gaap.live.genesys.com", Path: "/MattGDF"},
+							URL: &url.URL{Scheme: "https", Host: "newpod-gaap.live.genesys.com", Path: "/MattGDF/"},
 							Payload: struct {
 								Message string `json:"message"`
 							}{
