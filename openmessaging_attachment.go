f413cf16dd316ff6aa4956e5eb57b5bfd60f250a
Gildas Cherruel <gildas@breizh.org>
2022-04-22 19:04:33 +0900

    SendInboundAttachment should receive an OpenMessagingAttachment

--- openmessaging_attachment.go
+++ openmessaging_attachment.go
@@ -2,20 +2,59 @@ package gcloudcx
 
 import (
 	"encoding/json"
+	"mime"
 	"net/url"
+	"path"
+	"strings"
 
 	"github.com/gildas/go-core"
 	"github.com/gildas/go-errors"
+	"github.com/gildas/go-request"
+	nanoid "github.com/matoous/go-nanoid/v2"
 )
 
 type OpenMessageAttachment struct {
-	ID       string   `json:"id"`
+	ID       string   `json:"id,omitempty"`
 	Type     string   `json:"mediaType"`
 	URL      *url.URL `json:"-"`
 	Mime     string   `json:"mime,omitempty"`
 	Filename string   `json:"filename,omitempty"`
 	Text     string   `json:"text,omitempty"`
 	Hash     string   `json:"sha256,omitempty"`
+}
+
+func (attachment OpenMessageAttachment) WithContent(content *request.Content) *OpenMessageAttachment {
+	var attachmentType string
+	switch {
+	case len(content.Type) == 0:
+		attachmentType = "Link"
+	case strings.HasPrefix(content.Type, "audio"):
+		attachmentType = "Audio"
+	case strings.HasPrefix(content.Type, "image"):
+		attachmentType = "Image"
+	case strings.HasPrefix(content.Type, "video"):
+		attachmentType = "Video"
+	default:
+		attachmentType = "File"
+	}
+
+	attachment.Type = attachmentType
+	attachment.Mime = content.Type
+	attachment.Filename = content.Name
+	attachment.URL = content.URL
+
+	if attachmentType != "Link" && len(content.Name) == 0 {
+		fileExtension := path.Ext(content.URL.Path)
+		if content.Type == "audio/mpeg" {
+			fileExtension = ".mp3"
+		} else if fileExtensions, err := mime.ExtensionsByType(content.Type); err == nil && len(fileExtensions) > 0 {
+			fileExtension = fileExtensions[0]
+		}
+		fileID, _ := nanoid.New()
+		attachment.Filename = strings.ToLower(attachmentType) + "-" + fileID + fileExtension
+	}
+
+	return &attachment
 }
 
 // MarshalJSON marshals this into JSON
